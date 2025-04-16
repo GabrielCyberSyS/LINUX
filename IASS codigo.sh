0f1bc87689617bdd72fd7f3ca821efddf813f475
CyberSec <36765561+GabrielCyberSyS@users.noreply.github.com>
2025-04-16 20:47:49 -0300

    IASS codigo.sh

--- IASS codigo.sh
+++ IASS codigo.sh
@@ -15,17 +15,17 @@ groupadd GRP_SEC
 
 echo   "criando ususarios.."
 
-useradd  carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123)  -G GRP_ADM
-useradd  maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123)   -G GRP_ADM
-useradd  joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123)    -G GRP_ADM
+useradd carlos -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM && sudo passwd --expire carlos
+useradd maria -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM && sudo passwd --expire maria
+useradd joao -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM && sudo passwd --expire joao
 
-useradd  debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123)       -G GRP_VEN     
-useradd  sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123)   -G GRP_VEN           
-useradd  roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123)     -G GRP_VEN            
+useradd debora -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM && sudo passwd --expire joao
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM && sudo passwd --expire sebastiana
+useradd roberto -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM && sudo passwd --expire roberto
 
-useradd  josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123)  -G GRP_SEC          
-useradd  amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123)     -G GRP_SEC          
-useradd  rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123)   -G GRP_SEC            
+useradd josefina -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM && sudo passwd --expire josefina
+useradd amanda -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM && sudo passwd --expire amanda
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM && sudo passwd --expire rogerio
 
 echo "Especificando Permissoes de Usuario.."
 
