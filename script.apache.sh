e0a349edfbf983b60fefcd486fbccacbd28274ac
CyberSec <36765561+GabrielCyberSyS@users.noreply.github.com>
2025-04-20 19:50:55 -0300

    Create script.apache.sh

---
+++ script.apache.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo " Atualizando Servidor..."
+apt-get update
+apt-guet upgrade -y
+apt-get install apache2 -y
+apt-get instal unzip -y
+
+echo " Baixando app aplicacao..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/arquive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio
+cp-R * /var/www/html/
